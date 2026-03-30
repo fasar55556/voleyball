7da7d646c13a17d30dffd939b4cb05ad4c2ca945
fasar55556 <xanopov25.vb@gmail.com>
2026-03-30 23:16:20 +0300

    фикс

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/voleyball/",  // <--- ЭТО САМОЕ ВАЖНОЕ ДЛЯ GITHUB PAGES
   server: {
     host: "::",
     port: 8080,
